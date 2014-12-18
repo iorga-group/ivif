98bf7173c1401c5ae32b8f800b23c5eacaabaa75
Anthony OGIER <aogier@iorga.com>
2014-12-18 10:45:48 +0100

    First basic working version of AEType Form + Grid

---
+++ ivif-api/src/main/java/com/iorga/ivif/util/Generated.java
@@ -0,0 +1,12 @@
+package com.iorga.ivif.util;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({java.lang.annotation.ElementType.TYPE, java.lang.annotation.ElementType.METHOD, java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+public @interface Generated {
+}
