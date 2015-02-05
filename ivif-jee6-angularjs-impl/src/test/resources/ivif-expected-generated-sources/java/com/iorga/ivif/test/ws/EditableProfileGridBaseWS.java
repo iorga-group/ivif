e5de7d60129d3d66cc2d0c1dfcb06d7ab6a848e1
Anthony OGIER <aogier@iorga.com>
2015-02-05 16:03:48 +0100

    Forgot some expectations

---
+++ ivif-jee6-angularjs-impl/src/test/resources/ivif-expected-generated-sources/java/com/iorga/ivif/test/ws/EditableProfileGridBaseWS.java
@@ -0,0 +1,74 @@
+package com.iorga.ivif.test.ws;
+
+import com.iorga.ivif.ja.Generated;
+import com.iorga.ivif.ja.GridSearchParam;
+import com.iorga.ivif.ja.test.ProfileService;
+import com.iorga.ivif.test.entity.Profile;
+import com.iorga.ivif.test.service.ProfileBaseService;
+import com.mysema.query.SearchResults;
+import java.lang.Integer;
+import java.lang.String;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/editableProfileGrid")
+@Generated
+@Stateless
+public class EditableProfileGridBaseWS {
+
+    @Inject @Generated
+    private ProfileBaseService profileBaseService;
+
+    @Inject
+    private ProfileService saveService;
+
+
+    public static class EditableProfileGridSaveParam {
+        public String name;
+        public Integer id;
+    }
+    @POST
+    @Path("/save")
+    @Consumes("application/json")
+    @TransactionAttribute
+    public void save(List<EditableProfileGridSaveParam> saveParams) {
+        for (EditableProfileGridSaveParam saveParam : saveParams) {
+            // Search for this entityToSave
+            Profile entityToSave = profileBaseService.find(saveParam.id);
+            if (entityToSave == null) {
+                // Must create a new entityToSave
+                entityToSave = new Profile();
+            }
+            // Apply modifications
+            entityToSave.setName(saveParam.name);
+
+            // Ask for save
+            saveService.save(entityToSave);
+        }
+    }
+
+    public static class EditableProfileGridSearchResult extends EditableProfileGridSaveParam {
+
+        public EditableProfileGridSearchResult() {}
+        public EditableProfileGridSearchResult(String name, Integer id) {
+            this.name = name;
+            this.id = id;
+        }
+    }
+    public static class EditableProfileGridSearchFilter extends EditableProfileGridSearchResult {
+    }
+    public static class EditableProfileGridSearchParam extends GridSearchParam<EditableProfileGridSearchFilter> {}
+    @POST
+    @Path("/search")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public SearchResults<EditableProfileGridSearchResult> search(EditableProfileGridSearchParam searchParam) {
+        return profileBaseService.search(searchParam);
+    }
+}
---
+++ ivif-jee6-angularjs-impl/src/test/resources/ivif-expected-generated-sources/webapp/scripts/controllers/EditableProfileGridCtrl.js
@@ -0,0 +1,103 @@
+'use strict';
+
+angular.module('test')
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/editableProfileGrid', {
+                    templateUrl: 'templates/views/EditableProfileGrid.html',
+                    controller: 'EditableProfileGridCtrl'
+                });
+        }])
+    .controller('EditableProfileGridCtrl', ['$scope', 'ngTableParams', '$timeout', '$http', 'locationService', function($scope, ngTableParams, $timeout, $http, locationService) {
+        // Utils
+        function getIdForLine(line) {
+            return line.id;
+        }
+        // Declare actions
+        $scope.edit = function() {
+            $scope.$edit = true;
+            $scope.editedLinesById = {};
+            $scope.editableProfileGridTableParams.reload();
+        };
+        $scope.save = function() {
+            // Send only modified lines to server, thanks to http://stackoverflow.com/a/26975765/535203
+            var linesToSave = [];
+            angular.forEach($scope.editedLinesById, function(editedLine) {
+                if (!angular.equals(editedLine, editedLine.$original)) {
+                    linesToSave.push({
+                        name: editedLine.name,
+                        id: editedLine.id
+                    });
+                }
+            });
+            if (linesToSave.length > 0) {
+                // call save function
+                $http.post('api/editableProfileGrid/save', linesToSave).success(function() {
+                    // All was OK, let's ask to refresh the data
+                    $scope.cancel();
+                });
+            }
+        };
+        $scope.cancel = function() {
+            $scope.$edit = false;
+            $scope.editedLinesById = null;
+            $scope.editableProfileGridTableParams.reload();
+        };
+
+        // Init variables
+        function getData($defer, params) {
+            var sorting = {
+                ref: null,
+                type: null
+            };
+            var paramsSorting = params.sorting();
+            for (var field in paramsSorting) {
+                sorting.ref = field;
+                sorting.type = paramsSorting[field];
+            }
+            $http.post('api/editableProfileGrid/search', {
+                limit: params.count(),
+                offset: (params.page() - 1) * params.count(),
+                sorting: sorting,
+                filter: params.filter()
+            }).success(function(data) {
+                params.total(data.total);
+                var results = data.results;
+                if ($scope.$edit) {
+                    results = [];
+                    var editedLinesById = $scope.editedLinesById;
+                    angular.forEach(data.results, function(result) {
+                        var id = getIdForLine(result);
+                        var editedLine = editedLinesById[id];
+                        if (editedLine === undefined) {
+                            editedLine = angular.copy(result);
+                            editedLine.$original = result;
+                            editedLinesById[id] = editedLine;
+                        }
+                        results.push(editedLine);
+                    });
+                }
+                $defer.resolve(results);
+            });
+        }
+
+        if (!locationService.initializeController($scope)) {
+            $scope.editedLinesById = {};
+            $scope.editableProfileGridTableParams = new ngTableParams({
+                page: 1,
+                count: 10
+            }, {
+                total: 0, // length of data
+                getData: getData
+            });
+        } else {
+            // refresh getData function as the $scope is different from the original one
+            $scope.editableProfileGridTableParams.settings({
+                total: 0, // length of data
+                getData: getData
+            });
+        }
+        locationService.controllerInitialized('Profiles', $scope, ['editableProfileGridTableParams', 'editedLinesById']);
+    }])
+;
