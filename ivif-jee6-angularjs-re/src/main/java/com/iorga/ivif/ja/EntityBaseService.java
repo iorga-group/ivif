f216cae5c03349cb0e6a6fdb0262d3adee618c44
Gulati <pgulati@iorga.com>
2015-02-19 15:37:28 +0100

    dictionary grid error changes

--- ivif-jee6-angularjs-re/src/main/java/com/iorga/ivif/ja/EntityBaseService.java
+++ ivif-jee6-angularjs-re/src/main/java/com/iorga/ivif/ja/EntityBaseService.java
@@ -73,7 +73,7 @@ public abstract class EntityBaseService<E, I> extends PersistenceService {
     protected E toSingleResult(List<E> list) {
         if (list != null) {
             if (list.size() > 1) {
-              throw new NonUniqueResultException();
+              throw new NonUniqueResultException("Query produced " + list.size() + " results (expected 1)");
 //            	try{
 //            		list.
 //            	}catch (NonUniqueResultException ex){
