17b15a752c966dde0f486ec50fe461cfa0214965
Anthony OGIER <aogier@iorga.com>
2015-03-11 17:25:20 +0100

    Display a user friendly message in case of OptimisticLockException

--- ivif-jee6-angularjs-re/src/main/java/com/iorga/ivif/ja/EJBTransactionRollbackExceptionMapper.java
+++ ivif-jee6-angularjs-re/src/main/java/com/iorga/ivif/ja/EJBTransactionRollbackExceptionMapper.java
@@ -2,6 +2,7 @@ package com.iorga.ivif.ja;
 
 import javax.ejb.EJBTransactionRolledbackException;
 import javax.inject.Inject;
+import javax.persistence.OptimisticLockException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -14,11 +15,16 @@ public class EJBTransactionRollbackExceptionMapper implements ExceptionMapper<EJ
     @Inject
     private GlobalExceptionMapper globalExceptionMapper;
 
+    @Inject
+    private OptimisticLockExceptionMapper optimisticLockExceptionMapper;
+
     @Override
     public Response toResponse(EJBTransactionRolledbackException e) {
         final Throwable cause = e.getCause();
         if (cause instanceof FunctionalException) {
             return functionalExceptionMapper.toResponse((FunctionalException) cause);
+        } else if (cause instanceof OptimisticLockException) {
+            return optimisticLockExceptionMapper.toResponse((OptimisticLockException) cause);
         } else {
             return globalExceptionMapper.toResponse(e);
         }
